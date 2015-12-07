3c9d5044988cdac1717aed6b775e4be5d50dcb1a
tesyolan <tesfa1993@yahoo.com>
2015-12-07 17:50:04 +0300

    Update to the basic strucuture of the code organization.

--- cmt_tracker/src/cmt_tracker_node.cpp
+++ cmt_tracker/src/cmt_tracker_node.cpp
@@ -45,120 +45,157 @@ class TrackerCMT
 {
 
 //to hold the value of the image
-	cv::Mat conversion_mat_;
-	cv::Mat frame_gray; 
-	std::vector<cv::Mat> tracked_images; 
+  cv::Mat conversion_mat_;
+  cv::Mat frame_gray;
+  std::vector<cv::Mat> tracked_images;
 
 
-	ros::NodeHandle nh_;
-	image_transport::ImageTransport it_;
-	image_transport::Subscriber image_sub_;
-	image_transport::Publisher image_pub_;
-	ros::Publisher tracker_results;
-	ros::Subscriber face_results;
-	cmt_tracker::Tracker tracker_set; 
-	cmt_tracker::Faces face_locs;
-	std::vector<CMT> cmt;
-	std::vector<int> quality_of_tracker;
-	std::vector<cv::Rect> locations_of_trackers; //for setting the tracking to higher levels.
+  ros::NodeHandle nh_;
 
-	//Parameters for the CMT part of the code.
+  image_transport::ImageTransport it_;
+  image_transport::Subscriber image_sub_;
+  image_transport::Publisher image_pub_;
+
+  ros::Publisher tracker_results_pub;
+  ros::Subscriber face_results;
+
+  cmt_tracker::Tracker tracker_set;
+  cmt_tracker::Faces face_locs;
+  cmt_tracker::Trackers trackers_results;
+
+  std::vector<CMT> cmt;
+  std::vector<int> quality_of_tracker;
+
+  std::vector<cv::Rect> locations_of_trackers; //for setting the tracking to higher levels.
+
+
+  //Parameters for the CMT part of the code.
 public:
-	TrackerCMT() : it_(nh_)
-	{
-		//Here lies the essential parameters that we would set up to track the files.
-		//Initally till things get to function pretty well let's do this nothing. Just subscribe to the images and publishing topics.
-		nh_.getParam("camera_topic", subscribe_topic);
-		image_sub_ = it_.subscribe(subscribe_topic, 1, &TrackerCMT::imageCb, this);
-		face_subscriber = (nh).subscribe("face_locations", 1, &list_of_faces_update, this);
-		tracker_subscriber = (nh).subscribe("track_locations", 1, &set_tracker, this);
-		image_pub_ = it_.advertise("/transformed/images", 1);
-	}
-	/*
-	This  function is a callback fucntion that happens when an image update occurs.
-	*/
-	void imageCb(const sensor_msgs::ImageConstPtr& msg)
-	{
-		try
-		{
-			// First let cv_bridge do its magic
-			cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::RGB8);
-			conversion_mat_ = cv_ptr->image;
-		}
-		catch (cv_bridge::Exception& e)
-		{
-			try
-			{
-				// If we're here, there is no conversion that makes sense, but let's try to imagine a few first
-				cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg);
-				if (msg->encoding == "CV_8UC3")
-				{
-					// assuming it is rgb
-					conversion_mat_ = cv_ptr->image;
-				} else if (msg->encoding == "8UC1") {
-					// convert gray to rgb
-					cv::cvtColor(cv_ptr->image, conversion_mat_, CV_GRAY2RGB);
-				}  else {
-					// qWarning("ImageView.callback_image() could not convert image from '%s' to 'rgb8' (%s)", msg->encoding.c_str(), e.what());
-					std::cerr << "Error in the conversion of the iamge" << std::endl;
-					// ui_.image_frame->setImage(QImage());
-					return;
-				}
-			}
-			catch (cv_bridge::Exception& e)
-			{
-				std::cerr << "Error in the conversion of the iamge" << std::endl;
-				// qWarning("ImageView.callback_image() while trying to convert image from '%s' to 'rgb8' an exception was thrown (%s)", msg->encoding.c_str(), e.what());
-				// ui_.image_frame->setImage(QImage());
-				return;
-			}
-		}
-		//Do convert the image to gray scale as it's don't multiple times across other functions in the system. 
-		cv::cvtColor(conversion_mat_, frame_gray, cv::COLOR_BGR2GRAY);
-	}
+  TrackerCMT() : it_(nh_)
+  {
+    //Here lies the essential parameters that we would set up to track the files.
+    //Initally till things get to function pretty well let's do this nothing. Just subscribe to the images and publishing topics.
+    nh_.getParam("camera_topic", subscribe_topic);
+    image_sub_ = it_.subscribe(subscribe_topic, 1, &TrackerCMT::imageCb, this);
+    face_subscriber = (nh).subscribe("face_locations", 1, &list_of_faces_update, this);
+    tracker_subscriber = (nh).subscribe("track_locations", 1, &set_tracker, this);
+    image_pub_ = it_.advertise("/transformed/images", 1);
+    tracker_results_pub = nh_.advertise<cmt_tracker::Trackers>("tracker_results", 10);
 
-	void set_tracker(const cmt_tracker::Tracker& tracker_locations)
-	{
-		//A potentially high penality task is done here but it's to avoid latter dealing with uncorrectly set trackers. 
+  }
+  /*
+  This  function is a callback fucntion that happens when an image update occurs.
+  */
+  void imageCb(const sensor_msgs::ImageConstPtr& msg)
+  {
+    try
+    {
+      // First let cv_bridge do its magic
+      cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg, sensor_msgs::image_encodings::RGB8);
+      conversion_mat_ = cv_ptr->image;
+    }
+    catch (cv_bridge::Exception& e)
+    {
+      try
+      {
+        // If we're here, there is no conversion that makes sense, but let's try to imagine a few first
+        cv_bridge::CvImageConstPtr cv_ptr = cv_bridge::toCvShare(msg);
+        if (msg->encoding == "CV_8UC3")
+        {
+          // assuming it is rgb
+          conversion_mat_ = cv_ptr->image;
+        } else if (msg->encoding == "8UC1") {
+          // convert gray to rgb
+          cv::cvtColor(cv_ptr->image, conversion_mat_, CV_GRAY2RGB);
+        }  else {
+          // qWarning("ImageView.callback_image() could not convert image from '%s' to 'rgb8' (%s)", msg->encoding.c_str(), e.what());
+          std::cerr << "Error in the conversion of the iamge" << std::endl;
+          // ui_.image_frame->setImage(QImage());
+          return;
+        }
+      }
+      catch (cv_bridge::Exception& e)
+      {
+        std::cerr << "Error in the conversion of the image" << std::endl;
+        // qWarning("ImageView.callback_image() while trying to convert image from '%s' to 'rgb8' an exception was thrown (%s)", msg->encoding.c_str(), e.what());
+        // ui_.image_frame->setImage(QImage());
+        return;
+      }
+    }
+    //Do convert the image to gray scale as it's don't multiple times across other functions in the system.
+    cv::cvtColor(conversion_mat_, frame_gray, cv::COLOR_BGR2GRAY);
+  }
 
-		sensor_msgs::ImageConstPtr imageprocess= frame_gray; 
+  void set_tracker(const cmt_tracker::Tracker& tracker_location)
+  {
+    //A potentially high penality task is done here but it's to avoid latter dealing with uncorrectly set trackers.
 
-          if (!im_gray.empty() && candidate_faces[i].area() > 50)
-          {
-            //Now there must be some way to hold back setting up new tracker;
-            cmt.push_back(CMT());
-            cmt.back().consensus.estimate_rotation = true;
-            cmt.back().initialize(im_gray, candidate_faces[i]);
-          }
+    std::cout << "Initalizing tracker with values" << std::endl;
+    cv::Mat im_gray = frame_gray.clone(); //To avoid change when being run.
+    cv::Rect rect(tracker_location.pixel_lu.x, tracker_location.pixel_lu.y, tracker_location.width.data, tracker_location.height.data );
+    if (!im_gray.empty() && rect.area() > 50)
+    {
+      //Now there must be some way to hold back setting up new tracker;
+      cmt.push_back(CMT());
+      cmt.back().consensus.estimate_rotation = true;
+      cmt.back().initialize(im_gray, rect, tracker_location.tracker_name.data);
+    }
+    else
+    {
+      std::cout << "Not initialized" << std::endl;
+    }
+    std::cout << "Initalizing Done in the system. " << std::endl;
 
-		
-	}
+  }
 
-	void process()
-	{
-		//To avoid change during in process. 
-		sensor_msgs::ImageConstPtr imageprocess= frame_gray; 
+  void process()
+  {
+    std::cout << "Entered Processing" << std::endl;
+    cv::Mat im_gray = frame_gray().clone();
+    for (std::vector<CMT>::iterator v = cmt.begin(); v != cmt.end(); ++v)
+    {
+      //Clear all the previous results relating to the tracker.
+      (*v).processFrame(im_gray);
+      cv::Rect rect = (*v).bb_rot.boundingRect();
+      cmt_tracker::Tracker tracker;
+      tracker.pixel_lu.x = rect.x;
+      tracker.pixel_lu.y = rect.y;
+      tracker.pixel_lu.z = 0;
 
+      tracker.width.data = rect.width;
+      tracker.height.data = rect.height;
 
-	}
+      tracker.name.data = (*v).name;
 
-	void setTracker()
-	{
-		//This has one version where it set's based on the values sent from another controller
+      trackers_results.tracker_results.push_back(tracker);
 
-		//Another set from internal computation deciding what to track in the place of the system. 
-			
+      //Now let's publish the results of the tracker.
+    }
+    tracker_results_pub.publish(trackers_results); 
+    //This is a fucniton that performs that does the elements. 
 
+    std::cout<<"Outputed results of processing"<<std::endl; 
+    //Now publish the results of the tracking.
 
-		//Here one person sets what to track across time. 
-	}
+  }
+
+  void rules()
+  {
+
+  }
+
+  virtual void spinOnce ()
+  {
+    process()
+    spinOnce();
+  }
 
 };
 
 int main(int argc, char** argv)
 {
-	ros::init(argc, argv, "cmt_tracker");
-	TrackerCMT ic;
-	ros::spin();
-	return 0;
+  ros::init(argc, argv, "cmt_tracker");
+  TrackerCMT ic;
+  ic.spin();
+  return 0;
 }
